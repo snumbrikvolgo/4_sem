466dbe8c8169eae1d4261131adad414bf34d202d
Elizaveta Zazhigina <43406459+snumbrikvolgo@users.noreply.github.com>
2020-05-23 01:54:43 +0300

    kinda works

--- progress_check/buffer.h
+++ progress_check/buffer.h
@@ -7,134 +7,136 @@
 #define OUT 4*1024
 
 class Circular_buffer {
-public:
-	Circular_buffer() :
-		capacity_(CAPACITY)
+	public:
+		Circular_buffer() :
+			capacity_(CAPACITY)
 	{
-        buffer_ = new char [CAPACITY];
-        almost_full.second = almost_full.first.get_future();
-        almost_empty.second = almost_empty.first.get_future();
-    }
-
-	int put(int fd, int sz)
-	{
-
-        if (size() == capacity_)
-        {
-            printf("WRITE FULL\n");
-            almost_empty.second.get();
-
-            almost_empty.first = std::promise<int>();
-            almost_empty.second = almost_empty.first.get_future();
-        }
-        else if (size() > 0.8*capacity_)
-        {
-            printf("WRITE BIG\n");
-            almost_full.first.set_value(0);
-        }
-		int ch_read = read(fd, buffer_ + tail_, OUT);
-        if (ch_read == sz % capacity_)
-        {
-            almost_full.first.set_value(0);
-        }
-        mutex_.lock();
-        tail_ = (tail_ + ch_read) % capacity_;
-        mutex_.unlock();
-		// if(full_)
-		// {
-		// 	tail_ = (tail_ + 1) % capacity_;
-		// }
-
-		//head_ = (head_ + 1) % capacity_;
-
-		full_ = head_ == tail_;
-
-        //almost_full.first.set_value(0);
-        return ch_read;
-
+		buffer_ = new char [CAPACITY];
+		almost_full.second = almost_full.first.get_future();
+		almost_empty.second = almost_empty.first.get_future();
 	}
 
-	int get(int fd)
-	{
-		//std::lock_guard<std::mutex> lock(mutex_);
-        int ch_write = 0;
-
-        if(empty())
+		int put(int fd, int sz)
 		{
-            printf("GET EMPTY\n");
-			almost_full.second.get();
-            almost_full.first = std::promise<int>();
-            almost_full.second = almost_full.first.get_future();
+
+			if (size() == capacity_)
+			{
+				printf("WRITE FULL\n");
+				almost_empty.second.get();
+
+				almost_empty.first = std::promise<int>();
+				almost_empty.second = almost_empty.first.get_future();
+			}
+			else if (size() > 0.8*capacity_)
+			{
+				printf("WRITE BIG\n");
+				almost_full.first.set_value(0);
+			}
+			int ch_read = read(fd, buffer_ + tail_, OUT);
+
+			if (ch_read == sz % capacity_ || sz >= 0)
+			{
+				almost_full.first.set_value(0);
+			}
+			mutex_.lock();
+			tail_ = (tail_ + ch_read) % capacity_;
+			mutex_.unlock();
+			// if(full_)
+			// {
+			// 	tail_ = (tail_ + 1) % capacity_;
+			// }
+
+			//head_ = (head_ + 1) % capacity_;
+
+			full_ = head_ == tail_;
+
+			//almost_full.first.set_value(0);
+			return ch_read;
+
 		}
 
-        else if(size() < 0.2*capacity_)
-            {
-                printf("GET ALMOST EMPTY\n");
-                almost_empty.first.set_value(0);
-            }
-        // if (sz % capacity_ < 0.8*capacity_){
-        //
-        // }
-        if (size() < OUT)
-            ch_write = write(fd, buffer_ + head_, size());
-        else ch_write = write(fd, buffer_ + head_, OUT);
-        mutex_.lock();
-        head_ = (head_ + ch_write) % capacity_;
-        mutex_.unlock();
-		full_ = false;
-
-        return ch_write;
-	}
-
-	void reset()
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		head_ = tail_;
-		full_ = false;
-	}
-
-	bool empty() const
-	{
-		return (!full_ && (head_ == tail_));
-	}
-
-	bool full() const
-	{
-		return full_;
-	}
-
-	size_t capacity() const
-	{
-		return capacity_;
-	}
-
-	size_t size() const
-	{
-		size_t size = capacity_;
-
-		if(!full_)
+		int get(int fd)
 		{
-			if(tail_ >= head_)
+			//std::lock_guard<std::mutex> lock(mutex_);
+			int ch_write = 0;
+
+			if(empty())
 			{
-				size = tail_ - head_;
+				printf("GET EMPTY\n");
+				almost_full.second.get();
+				almost_full.first = std::promise<int>();
+				almost_full.second = almost_full.first.get_future();
 			}
-			else
+
+			else if(size() < 0.2*capacity_)
 			{
-				size = capacity_ + tail_ - head_;
+				printf("GET ALMOST EMPTY\n");
+				almost_empty.first.set_value(0);
 			}
+			// if (sz % capacity_ < 0.8*capacity_){
+			//
+			// }
+			if (size() < OUT)
+				ch_write = write(fd, buffer_ + head_, size());
+			else ch_write = write(fd, buffer_ + head_, OUT);
+
+			mutex_.lock();
+			head_ = (head_ + ch_write) % capacity_;
+			mutex_.unlock();
+			full_ = false;
+
+			return ch_write;
 		}
 
-		return size;
-	}
+		void reset()
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			head_ = tail_;
+			full_ = false;
+		}
 
-char *buffer_;
-private:
-    std::pair<std::promise<int>, std::future<int>> almost_full;
-    std::pair<std::promise<int>, std::future<int>> almost_empty;
-	std::mutex mutex_;
+		bool empty() const
+		{
+			return (!full_ && (head_ == tail_) && size() == 0);
+		}
 
-	size_t head_ = 0;
-	size_t tail_ = 0;
-	const size_t capacity_;
-	bool full_ = false;
+		bool full() const
+		{
+			return full_;
+		}
+
+		size_t capacity() const
+		{
+			return capacity_;
+		}
+
+		size_t size() const
+		{
+			size_t size = capacity_;
+
+			if(!full_)
+			{
+				if(tail_ >= head_)
+				{
+					size = tail_ - head_;
+				}
+				else
+				{
+					size = capacity_ + tail_ - head_;
+				}
+			}
+
+			return size;
+		}
+
+		char *buffer_;
+	private:
+		std::pair<std::promise<int>, std::future<int>> almost_full;
+		std::pair<std::promise<int>, std::future<int>> almost_empty;
+		std::mutex mutex_;
+
+		size_t head_ = 0;
+		size_t tail_ = 0;
+		const size_t capacity_;
+		bool full_ = false;
 };
--- progress_check/producer.cpp
+++ progress_check/producer.cpp
@@ -2,6 +2,7 @@
 #include <future>
 #include <thread>
 #include <unistd.h>
+#include <cstring>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -15,75 +16,73 @@ using namespace std;
 
 
 long size_of_file(int text);
-void show_file (char* name);
+void show_file (const char* name);
 
-int main(int argc, char* argv[])
+int main(int argc, const char* argv[])
 {
-    //Circular_buffer buf;
+	if (argc == 1)
+	{
+		const char* name = "stdin";
+		show_file(name);
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			show_file(argv[i]);
+		}
+	}
 
-    // FILE* file_1 = fopen(argv[1], "rb");
-    // FILE* file_2 = stdout;
-
-    // if (argc == 1)
-    // {
-    //     while (n != 0)
-    //     {
-    //         auto fut1 = async(std::launch::async,[](char* string){
-    //             return read(0, string, MAX_OUT);
-    //         }, string);
-    //
-    //         auto fut2 = async(std::launch::async,[&fut1](char* string){
-    //         return write(1, string, MAX_OUT);
-    //         }, string);
-    //     }
-    // }
-    // if (argc == 1)
-    // {
-    //     show_file(0);
-    // }
-
-    {
-        for (int i = 1; i < argc; i++)
-        		{
-        			show_file(argv[i]);
-        		}
-    }
-
-    return 0;
+	return 0;
 }
 
 
-void show_file (char* name)
+void show_file (const char* name)
 {
-	int symbols = 0;
-	int fd = open(name, O_RDONLY);
-	long sz = size_of_file(fd);
+	int fd = 0;
+	long sz = 0;
+	if (!strcmp(name, "stdin"))
+	{
+		fd = 0;
+		sz = -1;
 
-    Circular_buffer buf;
+	} else
+	{
+		fd = open(name, O_RDONLY);
+		sz = size_of_file(fd);
+	}
 
-    std::thread thread_read([](Circular_buffer& buf, int file_read, int sz) {
-        int readen = 0;
-        while(readen != sz)
-        {
-            int result = buf.put(file_read, sz);
-            readen += result;
-        }
-
-    }, std::ref(buf), fd, sz);
-
-    std::thread thread_write([](Circular_buffer& buf, int file_write, int sz) {
-        int written = 0;
-        while(written != sz)
-        {
-            int result = buf.get(1);
-            written += result;
-        }
-    }, std::ref(buf), fd, sz);
-
-    thread_read.join();
-    thread_write.join();
 	if (fd < 0)
+	{
 		perror ("No such file or directory\n");
+		close(fd);
+		return;
+	}
+
+	Circular_buffer buf;
+
+	std::thread thread_read([](Circular_buffer& buf, int file_read, int sz) {
+			int readen = 0;
+			while(readen != sz)
+			{
+			readen += buf.put(file_read, sz);
+			//readen += result;
+			}
+
+			}, std::ref(buf), fd, sz);
+
+	std::thread thread_write([](Circular_buffer& buf, int file_write, int sz) {
+			int written = 0;
+			while(written != sz)
+			{
+			written += buf.get(1);
+			//written += result;
+			}
+			}, std::ref(buf), fd, sz);
+
+	thread_read.join();
+	thread_write.join();
+
 	close (fd);
 }
 
