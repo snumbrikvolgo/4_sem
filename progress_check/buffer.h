a5acf78a0e08dd9617f752c728a8f0f9f5155b69
Elizaveta Zazhigina <43406459+snumbrikvolgo@users.noreply.github.com>
2020-05-22 23:44:44 +0300

    Add files via upload

--- progress_check/buffer.h
+++ progress_check/buffer.h
@@ -1,31 +1,46 @@
-#include <iostream>
 #pragma once
+#include <iostream>
+#include <utility>
+#include <future>
+
 #define CAPACITY 1024*1024
 #define OUT 4*1024
+
 class Circular_buffer {
 public:
 	Circular_buffer() :
 		capacity_(CAPACITY)
 	{
         buffer_ = new char [CAPACITY];
+        almost_full.second = almost_full.first.get_future();
+        almost_empty.second = almost_empty.first.get_future();
     }
 
 	int put(int fd)
 	{
+        mutex_.lock();
 		//std::lock_guard<std::mutex> lock(mutex_);
         //printf("I AM IN READ\n" );
 
-        if (full_)
+        if (size() == capacity_)
         {
+            almost_empty.second.get();
+
+            almost_empty.first = std::promise<int>();
+            almost_empty.second = almost_empty.first.get_future();
+        }
+        if (size() > 0.8*capacity_)
+        {
+            almost_full.first.set_value(0);
+            //almost_full.
             //wait for free
         }
-
         //printf("TAIL %d\n", tail_ );
 
 		int ch_read = read(fd, buffer_ + tail_, OUT);
-        mutex_.lock();
+
         tail_ = (tail_ + ch_read) % capacity_;
-        mutex_.unlock();
+
 		// if(full_)
 		// {
 		// 	tail_ = (tail_ + 1) % capacity_;
@@ -34,26 +49,35 @@ public:
 		//head_ = (head_ + 1) % capacity_;
 
 		full_ = head_ == tail_;
+        mutex_.unlock();
         return ch_read;
+
 	}
 
 	int get(int fd)
 	{
 		//std::lock_guard<std::mutex> lock(mutex_);
         int ch_write = 0;
+        mutex_.lock();
+
+        if(size() < 0.2*capacity_)
+        {
+            almost_empty.first.set_value(0);
+        }
 		if(empty())
 		{
-			//printf("EMPTY\n" );//return T();
-            return 0;
+			almost_full.second.get();
+            almost_full.first = std::promise<int>();
+            almost_full.second = almost_full.first.get_future();
 		}
         if (size() < OUT)
             ch_write = write(fd, buffer_ + head_, size());
         else ch_write = write(fd, buffer_ + head_, OUT);
-        mutex_.lock();
-        head_ = (head_ + ch_write) % capacity_;
-        mutex_.unlock();
-		full_ = false;
 
+        head_ = (head_ + ch_write) % capacity_;
+
+		full_ = false;
+        mutex_.unlock();
         return ch_write;
 	}
 
@@ -102,6 +126,9 @@ public:
 
 char *buffer_;
 private:
+    std::pair<std::promise<int>, std::future<int>> almost_full;
+    std::pair<std::promise<int>, std::future<int>> almost_empty;
+
 	std::mutex mutex_;
 	size_t head_ = 0;
 	size_t tail_ = 0;
